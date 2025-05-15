5882c1ef007868b5f3a8e20fa59951ddd23ee7e1
Marco Antonio González Solis <gsmarcoglez@gmail.com>
2025-05-14 18:37:10 -0600

    second commit

--- db.php
+++ db.php
@@ -4,16 +4,15 @@ $dbname = "catalogo";
 $user = "gsmarco";
 $pass = "Olga0322";
 
-$host = "dpg-d076lls9c44c739o31lg-a.frankfurt-postgres.render.com";
-$dbname = "catalogo_t804";
-$user = "gsmarco";
-$pass = "mm0XqtKjmX3TNEVdujzXZFZQfHP5hNDe";
-
 $host = "dpg-d0hu5fa4d50c73ativ60-a.oregon-postgres.render.com";
 $dbname = "catalogo_e4mp";
 $user = "omar_diaz";
 $pass = "St709HGRxBiWkjESWFnr5xhy7WJuqvHh";
 
+$host = "dpg-d076lls9c44c739o31lg-a.frankfurt-postgres.render.com";
+$dbname = "catalogo_t804";
+$user = "gsmarco";
+$pass = "mm0XqtKjmX3TNEVdujzXZFZQfHP5hNDe";
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
